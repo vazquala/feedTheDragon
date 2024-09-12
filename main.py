597dc7a08ac59e26a2ae4ae2705a4fb4d7ddb176
Alan Vazquez <vazquala000@warren.k12.in.us>
2024-09-12 10:04:58 -0400

    added game assets

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed the Dragon")
 
+
+
 running = True
 while running:
     for event in pygame.event.get():
